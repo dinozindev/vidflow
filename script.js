a41d99b9b580e65f9389afb03d2776920b217714
dinozindev <pugplayzcraft@gmail.com>
2023-10-31 17:44:34 -0300

    finalizando aula 2. Instalando json-server e aplicando-o

---
+++ script.js
@@ -0,0 +1,20 @@
+const containerVideos = document.querySelector(".videos__container");
+
+async function buscarEMostrarVideos() {
+    const fetchAPI = await fetch("http://localhost:3000/videos")
+    const videos = await fetchAPI.json();
+    videos.forEach(video => {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+        <div class="descricao-video">
+            <img src="${video.imagem}" class="img-canal" alt="Logo do canal">
+            <h3 class="titulo-video">${video.titulo}</h3>
+            <p class="titulo-canal">${video.descricao}</p>
+        </div>
+        </li>
+        `
+    })
+}
+
+buscarEMostrarVideos();
