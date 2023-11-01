6a191055985f32b379aa84691b635598be7d71b9
dinozindev <pugplayzcraft@gmail.com>
2023-11-01 17:34:52 -0300

    finalizando aula 4. Adicionando filtro para categorias e filtro de pesquisa

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const containerVideos = document.querySelector(".videos__container");
 const barraDePesquisa = document.querySelector(".pesquisar__input");
+const botaoCategoria = document.querySelectorAll(".superior__item");
 
 async function buscarEMostrarVideos() {
     try {
@@ -16,6 +17,7 @@ async function buscarEMostrarVideos() {
             <img src="${video.imagem}" class="img-canal" alt="Logo do canal">
             <h3 class="titulo-video">${video.titulo}</h3>
             <p class="titulo-canal">${video.descricao}</p>
+            <p class="categoria" hidden>${video.categoria}</p>
         </div>
         </li>
         `
@@ -30,4 +32,47 @@ async function buscarEMostrarVideos() {
 }
 
 buscarEMostrarVideos();
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item")
+    let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+    if (barraDePesquisa.value != "") {
+        videos.forEach(video => {
+            if (!video.querySelector(".titulo-video").textContent.toLowerCase().includes(valorFiltro)) {
+                video.style.display = 'none';
+            } else {
+                video.style.display = 'block';
+            }
+        })
+    } else {
+        videos.forEach(video => {
+           video.style.display = 'block'; 
+        })
+        
+    }
+}
+
+botaoCategoria.forEach(botao => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelector(".videos__item");
+    videos.forEach(video => {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let filtroNome = filtro.toLowerCase();
+
+        if (!categoria.includes(filtroNome) && filtroNome != "Tudo") {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    })
+
+}
+
 
