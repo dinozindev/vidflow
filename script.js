a33800e45a4261fef50977d7e3ad59521dbaff22
dinozindev <pugplayzcraft@gmail.com>
2023-11-01 15:24:48 -0300

    finalizando aula 3. Tratando possiveis erros

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const containerVideos = document.querySelector(".videos__container");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 async function buscarEMostrarVideos() {
-    const fetchAPI = await fetch("http://localhost:3000/videos")
-    const videos = await fetchAPI.json();
-    videos.forEach(video => {
-        containerVideos.innerHTML += `
+    try {
+        const fetchAPI = await fetch("http://localhost:3000/videos")
+        const videos = await fetchAPI.json();
+        videos.forEach(video => {
+            if (video.categoria == "") {
+                throw new Error("Vídeo não tem categoria");
+            }
+            containerVideos.innerHTML += `
         <li class="videos__item">
         <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
         <div class="descricao-video">
@@ -14,7 +19,15 @@ async function buscarEMostrarVideos() {
         </div>
         </li>
         `
-    })
+        })
+    } catch (error) {
+        containerVideos.innerHTML = `
+            <p>Houve um erro ao carregar os vídeos. ${error}</p>
+        `
+    } finally {
+
+    }
 }
 
 buscarEMostrarVideos();
+
